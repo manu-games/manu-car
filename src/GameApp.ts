06e3a4c403965fd51e2f45314da76e4e55e635c7
Emmanuel <emmanuel.lazarte.012@gmail.com>
2021-07-30 23:29:42 -0300

    GameApp
    Implementamos la clase principal

--- src/GameApp.ts
+++ src/GameApp.ts
@@ -1,11 +1,16 @@
 import * as PIXI from 'pixi.js'
+import {Auto} from './Auto'
+
+type WorldObject = Auto
 
 export class GameApp{
-    private app: PIXI.Application
-    static stage: PIXI.Container
+    public static app: PIXI.Application
+    private width:number
+    private height:number
+    private entidades: Array<WorldObject> = []
 
     public constructor(_width:number, _height:number){
-        this.app = new PIXI.Application({
+        GameApp.app = new PIXI.Application({
             transparent: true,
             backgroundColor: 0x1099bb,
             width: _width,
@@ -13,9 +18,37 @@ export class GameApp{
             resolution: 1,
             antialias: true
         })
+        document.body.appendChild(GameApp.app.view)
 
-        GameApp.stage = this.app.stage
-        document.body.appendChild(this.app.view)
+        this.width = _width
+        this.height = _height
 
+        this.init()
+        GameApp.app.ticker.add(delta => this.update(delta))
+    }
+
+    private init(){
+        const velocidad = {vx:0, vy:0}
+        const auto = new Auto(this.width/2, this.height/2, velocidad)
+
+        this.entidades.push(auto)
+
+        GameApp.app.stage.addChild(auto.getSprite())
+    }
+
+    private update(delta){
+        this.entidades.forEach(entidad => {
+            entidad.update(delta)
+        })
+    }
+
+    public static getWidth():number{
+        return GameApp.app.renderer.width
+    }
+
+    public static getHeight():number{
+        return GameApp.app.renderer.height
     }
 }
+
+
