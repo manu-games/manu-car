c960018708810213445073eeb001ebbc198a82bd
Emmanuel <emmanuel.lazarte.012@gmail.com>
2021-08-03 20:15:43 -0300

    Escenario - Abstracción Autopista
    Extendemos los escenarios para una nueva abstracción Autopista que tiene un Container personalizado

--- src/Escenario.ts
+++ src/Escenario.ts
@@ -1,20 +1,12 @@
 import * as PIXI from 'pixi.js'
 import {GameApp} from './GameApp'
+import {ManuContainer} from './Pixi.mixin'
 
-export class Escenario{
-    private sprite: PIXI.TilingSprite
-    private velocidadAcelerada:number = 1
-    private velocidadAnimacion:number = 1
-
-    public constructor(texture: PIXI.Texture){
-        this.sprite = new PIXI.TilingSprite(
-            texture,
-            GameApp.getWidth(),
-            GameApp.getHeight()
-        )
-
-        this.velocidadAnimacion = (GameApp.nivel / 2) * this.velocidadAcelerada
-    }
+export abstract class Escenario{
+    protected sprite: PIXI.TilingSprite
+    protected velocidadAcelerada:number = 1
+    protected velocidadAnimacion:number = 1
+    protected zonaJugable: ManuContainer
 
     public getSprite(): PIXI.TilingSprite{
         return this.sprite
@@ -28,7 +20,38 @@ export class Escenario{
         this.velocidadAnimacion = velocidadAnimacion
     }
 
+    public setZonaJugable(zonaJugable: ManuContainer):void{
+        this.zonaJugable = zonaJugable
+    }
+
+    public getZonaJugable(): ManuContainer{
+        return this.zonaJugable
+    }
+
     public update(delta:number):void{
         this.sprite.tilePosition.y += this.velocidadAnimacion * this.velocidadAcelerada
+    }
+}
+
+export class Autopista extends Escenario{
+    private lateralAutopista = 136
+
+    public constructor(texture: PIXI.Texture, zonaJugable: ManuContainer){
+        super()
+
+        zonaJugable.myWidth = GameApp.getWidth() - this.lateralAutopista*2
+        zonaJugable.myHeight = GameApp.getHeight()
+        // desplazamos a la derecha para centrarlo al sprite del escenario
+        zonaJugable.x = this.lateralAutopista
+        this.zonaJugable = zonaJugable
+
+        this.sprite = new PIXI.TilingSprite(
+            texture,
+            GameApp.getWidth(),
+            GameApp.getHeight()
+        )
+
+        this.velocidadAnimacion = (GameApp.nivel / 2) * this.velocidadAcelerada
+
     }
 }
